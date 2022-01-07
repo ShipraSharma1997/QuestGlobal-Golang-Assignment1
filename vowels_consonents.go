e3febabc879d2b5c975a6ff2df8986342034ade9
ShipraSharma1997 <41231443+ShipraSharma1997@users.noreply.github.com>
2022-01-07 12:14:00 +0530

    Update vowels_consonents.go

--- vowels_consonents.go
+++ vowels_consonents.go
@@ -4,19 +4,30 @@ package main
 
 import "fmt"
 
-func main() {
-	str := "kiran@"
-	count := 0
-	for _, ch := range str {
-		switch ch {
-		case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+func main() {  
+ str := "kiran@"  
+ vowel := 0
+ consonent := 0
+ special_character := 0
+ 
+ for _, ch := range str {  
+  switch ch {  
+  case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':  
+   vowel++  
+   
+   case '!', '@', '#', '$', '%', '^', '&', '*','(', ')':  
+   special_character++
+   
+    case 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k','l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'w', 'v','x','y','z':  
+   consonent++
+ 
+  }
+ 
+ }  
+ fmt.Printf(" %s string contains vowels count: %d\n", str, vowel)  
+ fmt.Printf(" %s string contains constant count: %d\n", str, consonent)
+    fmt.Printf(" %s string contains special character count: %d\n", str, special_character)  
 
-			count++
-		}
 
-	}
-	fmt.Printf(" %s string contains vowels count: %d\n", str, count)
-	fmt.Printf(" %s string contains constant count: %d\n", str, count)
-	fmt.Printf(" %s string contains special character count: %d\n", str, count)
-
-}
+ 
+}  
